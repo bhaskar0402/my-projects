08725dbb916a2c684f491194e0461d666efdc905
“bhaskar <bhaskaradhikari0@gmail.com>
2023-12-28 23:19:24 +0530

    Your commit message

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-
+E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
 
     requirements=[]
@@ -9,7 +9,9 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace('\n',"") for req in requirements]
-
+        
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
     return requirements
 
 setup(
