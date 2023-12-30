de80a99311ea6d0427532afdd19d6c943bcaaa78
“bhaskar <bhaskaradhikari0@gmail.com>
2023-12-30 15:20:13 +0530

    “mlflow“

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## end to end health insurence predict project 
+
+MLFLOW_TRACKING_URI=https://dagshub.com/bhaskar0402/my-projects.mlflow \
+MLFLOW_TRACKING_USERNAME=bhaskar0402 \
+MLFLOW_TRACKING_PASSWORD=6afb31e3d3b5c430ed9aad06aef79c2c3b664d23 \
+python script.py
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,9 +1,9 @@
 import os
 import sys
 from dataclasses import dataclass
-#from urllib.parse import urlparse
-#import mlflow
-#import mlflow.sklearn
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 import numpy as np
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from sklearn.ensemble import (
@@ -77,6 +77,31 @@ class ModelTrainer:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
+            # mlflow
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                mlflow.set_registry_uri("https://dagshub.com/bhaskar0402/my-projects.mlflow")
+                tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model_name)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
             save_object(
                file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
